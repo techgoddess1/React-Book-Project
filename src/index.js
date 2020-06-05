46038ce9640df4a8a1a15b1d195f65e723ef61c7
Frances Oparaocha <francesoparaocha@gmail.com>
2020-06-06 00:46:20 +0100

    Initial Commit for a mini book project as I learn how to work with react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+function Books() {
+  return (
+    <section className="books" >
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+      return <article className='book' >
+        <CoverImage/>
+        <Title/>
+        <Author/>
+      </article>
+};
+const CoverImage = () => ( 
+  <img width='200' src="https://m.media-amazon.com/images/I/51AQQAR2A1L._AC_UL480_FMwebp_QL65_.jpg" alt="peter rabbit"
+/>
 );
+const Title = () => <h1>Peter Rabbit</h1>
+const Author = () => <p>by Beatrix Potter</p>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+ReactDom.render(<Books/>, document.getElementById("root"));
+
